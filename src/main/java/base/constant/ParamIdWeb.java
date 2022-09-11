4e814a866fd15ecd9c884c7e40727982f4cce131
chakekaba <chakekabatm@gmail.com>
2022-09-11 20:03:33 +0900

    属性/ID用定数クラス - 共通データ保持クラス用のsession属性追加

--- src/main/java/base/constant/ParamIdWeb.java
+++ src/main/java/base/constant/ParamIdWeb.java
@@ -1,6 +1,14 @@
 package base.constant;
 
+/**
+ * 属性/ID用定数クラス
+ * @author kohei kajiki
+ *
+ */
 public class ParamIdWeb {
+	
+	/** 共通データ保持クラス **/
+	public static final String COM_DATA = "comData";
 
 	public static class ViewLogin {
 		
