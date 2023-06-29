2545a32bc64ccb8224917c154f5b1cef453698a7
chakekaba <chakekabatm@gmail.com>
2023-06-29 19:56:32 +0900

    Junitテスト初回作成

---
+++ src/test/java/logic/sv/LogicLoginUT.java
@@ -0,0 +1,123 @@
+package logic.sv;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.lang.reflect.Method;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import base.constant.ResultConstant;
+import base.model.MdlCommonData;
+import logic.sv.model.MdlLogicLoginIn;
+import logic.sv.model.MdlLogicLoginOut;
+
+class LogicLoginUT {
+
+	// 使用インスタンス呼出
+	public MdlCommonData comData;
+	public LogicLogin logicLogin;
+	public MdlLogicLoginIn inputData;
+	public MdlLogicLoginOut outputData;
+	Method method;
+
+	@BeforeEach
+	public void createNewInstance() {
+		comData = new MdlCommonData();
+		logicLogin = new LogicLogin();
+	}
+
+	@Nested
+	@DisplayName("checkInputDataメソッド")
+	class checkInputDataTest {
+
+		@BeforeEach
+		public void defMdlData () throws Exception {
+			inputData = new MdlLogicLoginIn();
+			outputData = new MdlLogicLoginOut();
+
+			// checkInputDataメソッド取得
+			method = LogicLogin.class.getDeclaredMethod(
+					"checkInputData",
+					MdlLogicLoginIn.class,
+					MdlCommonData.class);
+			method.setAccessible(true);
+		}
+
+		/**
+		 * .@MethodSourceの使用例
+		 * @param name
+		 * @param pass
+		 * @param msg
+		 * @throws Exception
+		 */
+		@ParameterizedTest
+		@MethodSource("dataTestInvalid")
+		@DisplayName("入力値エラーが1件のみ発生する場合")
+		public void testInvalid(
+				String name,
+				String pass,
+				String msg) throws Exception {
+
+			// 入力データ設定
+			inputData.setUserName(name);
+			inputData.setPassword(pass);
+
+			method.invoke(logicLogin, inputData, comData);
+
+			// 実行結果判定
+			assertEquals(ResultConstant.LOGIC_ERROR, comData.getResult());
+
+			// エラー情報件数
+			assertEquals(1, comData.getErrorCnt());
+
+			// メッセージ情報判定
+			MdlCommonData.ErrorData errorData = comData.getErrorDataList().get(0);
+			assertEquals(msg, errorData.getMessage());
+			assertTrue(errorData.isWARNING());
+		}
+
+		static Stream<Arguments> dataTestInvalid() {
+			return Stream.of(
+					Arguments.arguments(null, "_pass", "必須チェックエラー：ユーザ名"),
+					Arguments.arguments("", "_pass", "必須チェックエラー：ユーザ名"),
+					Arguments.arguments(" ", "_pass", "必須チェックエラー：ユーザ名"),
+					Arguments.arguments("_name", null, "必須チェックエラー：パスワード"),
+					Arguments.arguments("_name", "", "必須チェックエラー：パスワード"),
+					Arguments.arguments("_name", " ", "必須チェックエラー：パスワード")
+			);
+		}
+
+		/**
+		 * .@CsvSourceの使用例
+		 * @param name
+		 * @param pass
+		 */
+		@ParameterizedTest
+		@CsvSource({
+			"'a','b'",
+			"'sssssssss', 'xxxxxxxxxx'"
+		})
+		public void testValid(String name, String pass) {
+
+			// 入力データ設定
+			inputData.setUserName(name);
+			inputData.setPassword(pass);
+
+			// 実行結果判定
+			assertEquals(ResultConstant.NORMAL, comData.getResult());
+
+			// エラー情報件数
+			assertEquals(0, comData.getErrorCnt());
+		}
+
+	}
+
+
+}
